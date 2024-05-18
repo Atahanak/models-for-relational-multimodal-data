d998a4c97b975c2279a46c4d99147e361bf6c86d
Dragomir Drashkov <D.D.Drashkov@student.tudelft.nl>
2024-05-18 16:58:57 +0200

    fused FT-Transformer and PNA

--- src/nn/models/__init__.py
+++ src/nn/models/__init__.py
@@ -1,7 +1,9 @@
 from .ft_transformer import FTTransformer
 from .fused import FTTransformerGINeFused
+from .fused_ft_transformer_pna import FTTransformerPNAFused
 
 __all__ = [
     "FTTransformer",
-    "FTTransformerGINeFused"
+    "FTTransformerGINeFused",
+    "FTTransformerPNAFused"
 ]
