28a709afc91c8a5afb7ac4564614fa104948d223
Vasco de Graaff <vdegraaff@bunq.com>
2024-05-31 01:26:19 +0200

    update env

--- self_supervised.py
+++ self_supervised.py
@@ -390,7 +390,7 @@ def calc_loss(pred: torch.Tensor, y: torch.Tensor) -> Tuple[torch.Tensor, Tuple[
 
 
 def main(dataset="/data/Over-Sampled_Tiny_Trans-c.csv", run_name="self-supervised",
-         checkpoint="saved_models/self-supervised/ru_o8rmsna8_epoch_11.pth",
+         checkpoint=None,
          seed=42, batch_size=200, channels=128, num_layers=3, lr=2e-4, eps=1e-8, weight_decay=1e-3, epochs=10,
          data_split=[0.6, 0.2, 0.2], split_type="temporal", pretrain=["mask"],
          is_compile=False, testing=False, wand_dir="/mnt/data/"):
