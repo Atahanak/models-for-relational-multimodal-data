2198580d19a8cb562c444dfe033e9093b61c0955
Vasco de Graaff <vdegraaff@bunq.com>
2024-05-30 23:57:05 +0200

    update phplint

--- self_supervised.py
+++ self_supervised.py
@@ -1,35 +1,55 @@
-import fire
-import torch
+"""
+This module contains implementations for a self-supervised learning framework using
+PyTorch, WandB, and other utilities to train, evaluate, and manage experiments of machine
+learning models.
+"""
 import os
 import re
+from typing import Optional, Tuple, Set
+
+import fire
+import torch
 import torch.nn.functional as F
+from tqdm import tqdm
+from icecream import ic
+import wandb
+
+from transformers import get_inverse_sqrt_schedule
 from torch_frame import stype
 from torch_frame.data import DataLoader
-from torch_frame.nn import (
+from torch_frame.nn.encoder import (
     EmbeddingEncoder,
     LinearEncoder,
     TimestampEncoder,
 )
-from tqdm import tqdm
+from src.datasets import IBMTransactionsAML
+from src.nn.models.ft_transformer import FTTransformer
 from src.datasets.util.mask import PretrainType
-from transformers import get_inverse_sqrt_schedule
-
-from icecream import ic
-import wandb
 
 torch.set_float32_matmul_precision('high')
 torch.autograd.set_detect_anomaly(False)
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 num_numerical, num_categorical, num_columns = 0, 0, 0
 
-def parse_checkpoint(checkpoint):
-    # Construct the full file path
+
+def parse_checkpoint(checkpoint: str) -> [str, int]:
+    """
+    Parse the checkpoint file to extract the run identifier and the epoch number.
+
+    Args:
+        checkpoint (str): Path to the checkpoint file.
+
+    Returns:
+        Tuple[str, int]: A tuple containing the run identifier and the epoch number.
+
+    Raises:
+        ValueError: If the checkpoint file does not exist or has an invalid format.
+    """
     full_path = os.path.join(os.getcwd(), checkpoint)
-    # Check if the file exists
+
     if not os.path.isfile(full_path):
         raise ValueError('Checkpoint file does not exist')
 
-    # Extract information from the file name
     pattern = r"^run_(?P<run_id>[a-zA-Z0-9]+)_epoch_(?P<epoch>\d+)\.pth$"
     match = re.match(pattern, os.path.basename(checkpoint))
     if match:
@@ -41,13 +61,25 @@ def parse_checkpoint(checkpoint):
         raise ValueError('Checkpoint file has invalid format')
 
 
-def init_wandb(args, run_name: str, wandb_dir: str, run_id: str|None):
+def init_wandb(args: dict, run_name: str, wandb_dir: str, run_id: Optional[str]) -> object:
+    """
+    Initialize the Weights & Biases run for tracking and logging.
+
+    Args:
+        args (dict): Dictionary of parameters and their values for this run.
+        run_name (str): Name of the run.
+        wandb_dir (str): Directory to store Weights & Biases data.
+        run_id (Optional[str]): Existing run identifier for resuming, if any.
+
+    Returns:
+        object: An initialized wandb run object.
+    """
     wandb.login()
     if run_id is not None:
         run = wandb.init(
             id=run_id,
             entity="cse3000",
-            dir= wandb_dir,
+            dir=wandb_dir,
             mode="disabled" if args['testing'] else "online",
             project="rel-mm",
             name=run_name,
@@ -57,7 +89,7 @@ def init_wandb(args, run_name: str, wandb_dir: str, run_id: str|None):
     else:
         run = wandb.init(
             entity="cse3000",
-            dir= wandb_dir,
+            dir=wandb_dir,
             mode="disabled" if args['testing'] else "online",
             project="rel-mm",
             name=run_name,
@@ -66,8 +98,17 @@ def init_wandb(args, run_name: str, wandb_dir: str, run_id: str|None):
     wandb.log({"device": str(device)})
     return run
 
-def parse_pretrain_args(pretrain) -> set[PretrainType]:
 
+def parse_pretrain_args(pretrain) -> Set[PretrainType]:
+    """
+    Parse pretraining arguments into a set of pretraining types.
+
+    Args:
+        pretrain (list): List of pretrain argument strings.
+
+    Returns:
+        Set[PretrainType]: Set of pretraining types deduced from the arguments.
+    """
     pretrain_dict = {
         "mask": PretrainType.MASK,
         "mv": PretrainType.MASK_VECTOR,
@@ -81,9 +122,17 @@ def parse_pretrain_args(pretrain) -> set[PretrainType]:
     return pretrain_set
 
 
+def prepare_dataset(dataset_path: str, pretrain_set: Set[PretrainType]) -> IBMTransactionsAML:
+    """
+    Prepare the dataset for training by loading it and initializing necessary configurations.
 
-def prepare_dataset(dataset_path, pretrain_set):
-    from src.datasets import IBMTransactionsAML
+    Args:
+        dataset_path (str): Path to the dataset.
+        pretrain_set (Set[PretrainType]): Set of pretraining types to apply.
+
+    Returns:
+        IBMTransactionsAML: The prepared dataset.
+    """
     dataset = IBMTransactionsAML(root=os.getcwd() + dataset_path, pretrain=pretrain_set)
     ic(dataset)
     dataset.materialize()
@@ -96,8 +145,18 @@ def prepare_dataset(dataset_path, pretrain_set):
     ic(num_numerical, num_categorical, num_columns)
     return dataset
 
-# Set up data loaders
-def setup_data_loaders(dataset, batch_size):
+
+def setup_data_loaders(dataset: IBMTransactionsAML, batch_size: int) -> Tuple[DataLoader, DataLoader, DataLoader]:
+    """
+    Setup data loaders for the training, validation, and test sets.
+
+    Args:
+        dataset (IBMTransactionsAML): The dataset to split into loaders.
+        batch_size (int): The batch size for data loading.
+
+    Returns:
+        Tuple[DataLoader, DataLoader, DataLoader]: Data loaders for the training, validation, and test datasets.
+    """
     train_dataset, val_dataset, test_dataset = dataset.split()
     train_loader = DataLoader(train_dataset.tensor_frame, batch_size=batch_size, shuffle=True)
     val_loader = DataLoader(val_dataset.tensor_frame, batch_size=batch_size, shuffle=False)
@@ -110,9 +169,25 @@ def setup_data_loaders(dataset, batch_size):
     })
     return train_loader, val_loader, test_loader
 
-def initialize_model(dataset, device, channels, num_layers, pretrain_set, is_compile, checkpoint):
-    from src.nn.models.ft_transformer import FTTransformer
 
+def initialize_model(dataset: IBMTransactionsAML, device: torch.device, channels: int, num_layers: int,
+                     pretrain_set: Set[PretrainType], is_compile: bool, checkpoint: Optional[str]) -> torch.nn.Module:
+    """
+    Initialize the machine learning model with the specified architecture and settings,
+    optionally loading weights from a checkpoint.
+
+    Args:
+        dataset (IBMTransactionsAML): The dataset from which model configurations are derived.
+        device (torch.device): The device (CPU or GPU) to run the model on.
+        channels (int): The number of channels (or features) in the model's layers.
+        num_layers (int): The number of layers in the model.
+        pretrain_set (Set[PretrainType]): Set of pretraining types to apply.
+        is_compile (bool): Whether to compile the model using TorchScript.
+        checkpoint (Optional[str]): Path to the checkpoint file from which to load model weights.
+
+    Returns:
+        torch.nn.Module: The initialized (and possibly compiled) PyTorch model.
+    """
     model = FTTransformer(
         channels=channels,
         out_channels=None,
@@ -146,9 +221,19 @@ def initialize_model(dataset, device, channels, num_layers, pretrain_set, is_com
     return model
 
 
+def setup_optimizer(model: torch.nn.Module, lr: float, eps: float, weight_decay: float) -> torch.optim.Optimizer:
+    """
+    Set up the optimizer for the model training, using AdamW with specified parameters.
 
-# Set up optimizer and scheduler
-def setup_optimizer(model, lr, eps, weight_decay):
+    Args:
+        model (torch.nn.Module): The model for which the optimizer is to be set up.
+        lr (float): Learning rate for the optimizer.
+        eps (float): Epsilon parameter for the optimizer to improve numerical stability.
+        weight_decay (float): Weight decay (L2 penalty) to apply.
+
+    Returns:
+        torch.optim.Optimizer: Configured optimizer.
+    """
     no_decay = ['bias', 'LayerNorm.weight']
     optimizer_grouped_parameters = [
         {'params': [p for n, p in model.named_parameters() if not any(nd in n for nd in no_decay)],
@@ -162,7 +247,20 @@ def setup_optimizer(model, lr, eps, weight_decay):
     wandb.log({"learnable_params": learnable_params})
     return optimizer
 
-def train(model, train_loader, optimizer, epoch: int) -> float:
+
+def train(model: torch.nn.Module, train_loader: DataLoader, optimizer: torch.optim.Optimizer, epoch: int) -> float:
+    """
+    Train the model for one epoch using the provided data loader and optimizer.
+
+    Args:
+        model (torch.nn.Module): The model to train.
+        train_loader (DataLoader): The DataLoader providing training data.
+        optimizer (torch.optim.Optimizer): The optimizer to use.
+        epoch (int): The current epoch number.
+
+    Returns:
+        float: The average training loss for the epoch.
+    """
     model.train()
     loss_accum = loss_c_accum = loss_n_accum = total_count = t_c = t_n = 0
 
@@ -196,13 +294,26 @@ def train(model, train_loader, optimizer, epoch: int) -> float:
     return ((loss_c_accum / t_c) * (num_categorical / num_columns)) + (
             (loss_n_accum / t_n) * (num_numerical / num_columns))
 
+
 @torch.no_grad()
-def test(model, test_loader: DataLoader, dataset_name: str, epoch: int) -> float:
+def test(model: torch.nn.Module, test_loader: DataLoader, dataset_name: str, epoch: int) -> Tuple[float, float]:
+    """
+    Evaluate the model using the provided data loader and log performance metrics to wandb.
+
+    Args:
+        model (torch.nn.Module): The model to evaluate.
+        test_loader (DataLoader): The DataLoader providing test data.
+        dataset_name (str): The name of the dataset to log as part of the metrics.
+        epoch (int): The current epoch number for logging purposes.
+
+    Returns:
+        Tuple[float, float]: Tuple containing RMSE (root mean squared error) and accuracy.
+    """
     model.eval()
     accum_acc = accum_l2 = 0
     loss_c_accum = loss_n_accum = 0
     t_n = t_c = 0
-    with tqdm(test_loader, desc=f'Evaluating') as t:
+    with tqdm(test_loader, desc='Evaluating') as t:
         for tf in t:
             tf = tf.to(device)
             pred = model(tf)
@@ -220,7 +331,8 @@ def test(model, test_loader: DataLoader, dataset_name: str, epoch: int) -> float
                     accum_l2 += torch.square(ans[0] - pred[0][i][int(ans[1])])  #rmse
 
             # loss numerical
-            loss_c_mcm = ((loss_c_accum / t_c) * (num_categorical / num_columns)) + ((loss_n_accum / t_n) * (num_numerical / num_columns))
+            loss_c_mcm = (((loss_c_accum / t_c) * (num_categorical / num_columns)) +
+                          ((loss_n_accum / t_n) * (num_numerical / num_columns)))
             loss_c = loss_c_accum / t_c
             loss_n = loss_n_accum / t_n
             wandb.log({f"{dataset_name}_loss_mcm": loss_c_mcm,
@@ -228,8 +340,8 @@ def test(model, test_loader: DataLoader, dataset_name: str, epoch: int) -> float
                        f"{dataset_name}_loss_n": loss_n,
                        "epoch": epoch})
 
-            acc = accum_acc/t_c
-            rmse = torch.sqrt(accum_l2/t_n)
+            acc = accum_acc / t_c
+            rmse = torch.sqrt(accum_l2 / t_n)
             loss = (loss_c_accum / t_c) + (loss_n_accum / t_n)
             t.set_postfix(accuracy=f'{acc:.4f}',
                           rmse=f'{rmse:.4f}',
@@ -237,7 +349,6 @@ def test(model, test_loader: DataLoader, dataset_name: str, epoch: int) -> float
                           loss_c_mcm=f'{loss_c_mcm:.4f}',
                           loss_c=f'{loss_c:.4f}',
                           loss_n=f'{loss_n:.4f}')
-
 
         wandb.log({f"{dataset_name}_accuracy": accum_acc / t_c,
                    f"{dataset_name}_rmse": rmse,
@@ -251,7 +362,18 @@ def test(model, test_loader: DataLoader, dataset_name: str, epoch: int) -> float
         return [rmse, acc]
 
 
-def calc_loss(pred, y):
+def calc_loss(pred: torch.Tensor, y: torch.Tensor) -> Tuple[torch.Tensor, Tuple[float, int], Tuple[float, int]]:
+    """
+    Calculate loss for a batch of predictions and corresponding true values.
+
+    Args:
+        pred (torch.Tensor): The predictions tensor.
+        y (torch.Tensor): The true values tensor.
+
+    Returns:
+        Tuple[torch.Tensor, Tuple[float, int], Tuple[float, int]]: The computed loss, a tuple containing accumulated
+        categorical loss and count, and a tuple containing accumulated numerical loss and count.
+    """
     accum_n = accum_c = t_n = t_c = 0
     for i, ans in enumerate(y):
         # ans --> [val, idx]
@@ -267,10 +389,11 @@ def calc_loss(pred, y):
     return (accum_n / t_n) + torch.sqrt(accum_c / t_c), (accum_c, t_c), (accum_n, t_n)
 
 
-def main(dataset="/data/Over-Sampled_Tiny_Trans-c.csv", run_name="self-supervised", checkpoint="saved_models/self-supervised/run_o8rmsna8_epoch_11.pth",
-         testing=False, seed=42,batch_size=200, channels=128, num_layers=3, data_split=[0.6, 0.2, 0.2], split_type="temporal",
-         pretrain=["mask"], is_compile=False, lr=2e-4, eps=1e-8, weight_decay=1e-3, epochs=10, wand_dir="/mnt/data/"):
-
+def main(dataset="/data/Over-Sampled_Tiny_Trans-c.csv", run_name="self-supervised",
+         checkpoint="saved_models/self-supervised/ru_o8rmsna8_epoch_11.pth",
+         seed=42, batch_size=200, channels=128, num_layers=3, lr=2e-4, eps=1e-8, weight_decay=1e-3, epochs=10,
+         data_split=[0.6, 0.2, 0.2], split_type="temporal", pretrain=["mask"],
+         is_compile=False, testing=False, wand_dir="/mnt/data/"):
     args = {
         "testing": testing,
         "seed": seed,
