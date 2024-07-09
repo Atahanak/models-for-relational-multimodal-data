2358bb3f73e25caf83b994d2eebcb17c55697994
hcagri <hcagri98@gmail.com>
2024-07-09 12:57:31 +0200

    model adaptation for supervised

--- src/nn/gnn/model.py
+++ src/nn/gnn/model.py
@@ -8,14 +8,16 @@ from .decoder import LinkPredHead
 
 
 class GINe(torch.nn.Module):
-    def __init__(self, num_features, num_gnn_layers, n_classes=2, 
-                n_hidden=128, edge_updates=False, residual=True, 
-                edge_dim=None, dropout=0.0, final_dropout=0.5):
+    def __init__(self, 
+                num_features=1, 
+                num_gnn_layers=2,
+                n_hidden=100, 
+                edge_updates=False, 
+                edge_dim=None):
         super().__init__()
         self.n_hidden = n_hidden
         self.num_gnn_layers = num_gnn_layers
         self.edge_updates = edge_updates
-        self.final_dropout = final_dropout
 
         self.node_emb = nn.Linear(num_features, n_hidden)
         self.edge_emb = nn.Linear(edge_dim, n_hidden)
@@ -29,37 +31,27 @@ class GINe(torch.nn.Module):
                 nn.ReLU(), 
                 nn.Linear(self.n_hidden, self.n_hidden)
                 ), edge_dim=self.n_hidden)
-            if self.edge_updates: 
-                self.emlps.append(nn.Sequential(
-                    nn.Linear(3 * self.n_hidden, self.n_hidden),
-                    nn.ReLU(),
-                    nn.Linear(self.n_hidden, self.n_hidden),
-                ))
+            if self.edge_updates: self.emlps.append(nn.Sequential(
+                nn.Linear(3 * self.n_hidden, self.n_hidden),
+                nn.ReLU(),
+                nn.Linear(self.n_hidden, self.n_hidden),
+            ))
             self.convs.append(conv)
             self.batch_norms.append(BatchNorm(n_hidden))
 
-        # self.decoder = nn.Sequential(Linear(n_hidden*3, 50), nn.ReLU(), nn.Dropout(self.final_dropout),Linear(50, 25), nn.ReLU(), nn.Dropout(self.final_dropout),
-        #                       Linear(25, n_classes))
-        self.decoder = LinkPredHead(n_classes=n_classes, n_hidden=n_hidden, final_dropout=final_dropout)
 
-    def forward(self, x, edge_index, edge_attr, pos_edge_index, pos_edge_attr, neg_edge_index, neg_edge_attr):
+    def forward(self, x, edge_index, edge_attr):
+        src, dst = edge_index
 
         x = self.node_emb(x)
-        edge_attr = self.edge_emb(edge_attr)
-        pos_edge_attr = self.edge_emb(pos_edge_attr)
-        neg_edge_attr = self.edge_emb(neg_edge_attr)
+        edge_attr = self.edge_emb(edge_attr.view(edge_attr.shape[0], -1))
 
         for i in range(self.num_gnn_layers):
             x = (x + F.relu(self.batch_norms[i](self.convs[i](x, edge_index, edge_attr)))) / 2
             if self.edge_updates: 
-                src, dst = edge_index
                 edge_attr = edge_attr + self.emlps[i](torch.cat([x[src], x[dst], edge_attr], dim=-1)) / 2
 
-        #x = x[edge_index.T].reshape(-1, 2 * self.n_hidden).relu()
-        #x = torch.cat((x, edge_attr.view(-1, edge_attr.shape[1])), 1)
-        #out = x
-        
-        return self.decoder(x, pos_edge_index, pos_edge_attr, neg_edge_index, neg_edge_attr)
+        return x, edge_attr
     
     def reset_parameters(self):
         for conv in self.convs:
@@ -69,7 +61,6 @@ class GINe(torch.nn.Module):
                     emlp.reset_parameters()
         self.node_emb.reset_parameters()
         self.edge_emb.reset_parameters()
-        self.decoder.reset_parameters()
     
 class PNA(torch.nn.Module):
     def __init__(self, num_features, num_gnn_layers, n_classes=2, 
