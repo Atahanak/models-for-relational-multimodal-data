cbae4c116360bd10220225973ebc9bb96ef52cfb
Atahanak <t.a.a.akyildiz@tudelft.nl>
2024-07-26 16:28:37 +0200

    fix

--- src/nn/models/__init__.py
+++ src/nn/models/__init__.py
@@ -1,11 +1,9 @@
 from .ft_transformer import FTTransformer
-from .fused_head import TABGNNFusedHead
 from .fused import TABGNNFused
 from .tabgnn import TABGNN
 
 __all__ = [
     "FTTransformer",
-    "TABGNNFusedHead",
     "TABGNNFused",
     "TABGNN",
 ]
